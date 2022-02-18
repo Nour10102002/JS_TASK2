90fc4e4c95c4d709e85bf160f5b9dd09ba9431c6
Nourhan Ayman <83882344+Nour10102002@users.noreply.github.com>
2022-02-18 19:33:04 +0200

    edit js file

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 function calculateTimezone() {
     var timeNow = parseInt(document.getElementById("timeNow").value);
     var answer = document.getElementById("answer");
+    var UTC = document.getElementById("utc") 
     
     var timeZone = timeNow-12;
+    if(timeZone>=0) {
+        UTC.innerHTML = "UTC +";
+    }
+    else {
+        UTC.innerHTML = "UTC "
+    }
     answer.innerHTML = `${timeZone}`;
 }
